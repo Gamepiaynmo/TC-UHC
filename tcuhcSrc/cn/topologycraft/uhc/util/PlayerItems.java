cc45cced778641c4b35087d9e0c3ed818539fd68
Fallen_Breath <yqx791125833@gmail.com>
2021-05-14 22:33:00 +0800

    Refactored PlayerItems and added more custom morals - Used builder to create player items - Added a few custom morals - Added method `setSingleLore` in ItemStack - Added `/uhc givemorals` to get all - Fixed `/uhc deathpos` showing "Unknown command"

--- tcuhcSrc/cn/topologycraft/uhc/util/PlayerItems.java
+++ tcuhcSrc/cn/topologycraft/uhc/util/PlayerItems.java
@@ -1,45 +1,115 @@
 package cn.topologycraft.uhc.util;
 
 import com.google.common.collect.Maps;
+import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Enchantments;
 import net.minecraft.init.Items;
 import net.minecraft.init.PotionTypes;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionType;
 import net.minecraft.potion.PotionUtils;
 
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class PlayerItems {
-	
-	private static Map<String, ItemStack> items = Maps.newHashMap();
-	
+
+	private static final Map<String, ItemStack> items = Maps.newLinkedHashMap();
+
 	public static ItemStack getPlayerItem(String playerName) {
 		ItemStack stack = items.get(playerName);
 		if (stack == null) stack = new ItemStack(Items.PAPER);
 		else stack = stack.copy();
-		if (!stack.hasDisplayName())
-			stack.setStackDisplayName(playerName + "'s moral(jie) integrity(cao)");
+		String moral = playerName + "'s moral(jie) integrity(cao)";
+		if (!stack.hasDisplayName()) stack.setStackDisplayName(moral);
+		else stack.setSingleLore(moral);
 		if (stack.getEnchantmentTagList().tagCount() == 0)
 			stack.addEnchantment(Enchantments.SHARPNESS, 1);
 		return stack;
 	}
+
+	// for command /uhc givemorals
+	public static void dumpMoralsToPlayer(EntityPlayer player) {
+		items.keySet().forEach(name -> player.inventory.addItemStackToInventory(getPlayerItem(name)));
+	}
 	
 	static {
-		ItemStack stack = null;
-		stack = new ItemStack(Items.POTIONITEM);
-		PotionUtils.addPotionToItemStack(stack, PotionTypes.STRONG_POISON);
-		items.put("hungryartist_", stack.setStackDisplayName("hungryartist_'s holy water"));
+		items.put("hungryartist_", Builder.create(Items.POTIONITEM).potion(PotionTypes.STRONG_POISON).named("hungryartist_'s holy water").get());
 		items.put("_Flag_E_", new ItemStack(Blocks.RED_FLOWER));
 		items.put("Spring0809", new ItemStack(Blocks.TNT));
 		items.put("fire_duang_duang", new ItemStack(Items.COOKED_FISH).setStackDisplayName("fire_duang_duang's salted fish"));
-		stack = new ItemStack(Items.WOODEN_SWORD, 1, 59);
-		stack.addEnchantment(Enchantments.SHARPNESS, 10);
-		stack.getTagCompound().setInteger("RepairCost", 100);
-		items.put("Keviince", stack);
+		items.put("Keviince", Builder.create(Items.WOODEN_SWORD, 59).enchant(Enchantments.SHARPNESS, 10).expensive().get());
 		items.put("Dazo66", new ItemStack(Blocks.YELLOW_FLOWER));
 		items.put("Gamepiaynmo", new ItemStack(Items.COAL));
 		items.put("CCS_Covenant", new ItemStack(Items.COOKIE).setStackDisplayName("Crispy Crispy Shark!"));
+		items.put("Lancet_Corgi", Builder.create(Items.IRON_SWORD, 250).enchant(Enchantments.SHARPNESS, 5).expensive().get());
+		items.put("ajisai_iii", new ItemStack(Items.CHICKEN));
+		items.put("Aschin", Builder.create(Items.WHEAT).named("comymy").get());
+		items.put("Dou_Bi_Long", Builder.create(Blocks.DRAGON_EGG).named("longbao no egg").enchant(Enchantments.UNBREAKING, 10).enchant(Enchantments.MENDING, 1).get());
+		items.put("minamotosan", Builder.create(Items.ROTTEN_FLESH).named("spicy strip").get());
+		items.put("zi_nv", Builder.create(Blocks.DOUBLE_PLANT, 2).named("mY lIVe").enchant(Enchantments.EFFICIENCY, 11).get());
+		items.put("CallMeLecten", Builder.create(Items.GLASS_BOTTLE).named("oxygen").get());
+		items.put("HG_Fei", Builder.create(Items.POTIONITEM).named("hydrofluoric acid").get());
+		items.put("hai_dan", Builder.create(Items.EGG).named("sea egg").get());
+		items.put("Fallen_Breath", Builder.create(Items.LEATHER_CHESTPLATE).mani(s -> Items.LEATHER_CHESTPLATE.setColor(s, 16742436)).named("fox fur coat").get());
+		items.put("Sanluli36li", Builder.create(Items.TNT_MINECART).named("36li's self-destruct-car").enchant(Enchantments.FORTUNE, 3).get());
+		items.put("shamreltuim", Builder.create(Items.FISH, 3).get());  // puffer fish
+		items.put("YtonE", Builder.create(Items.POTIONITEM).potion(PotionTypes.EMPTY).get());  // uncraftable potion
 	}
 
+	private static class Builder
+	{
+		private final ItemStack itemStack;
+
+		private Builder(ItemStack itemStack) {
+			this.itemStack = itemStack;
+		}
+
+		private static Builder create(Item item, int meta) {
+			return new Builder(new ItemStack(item, 1, meta));
+		}
+
+		private static Builder create(Item item) {
+			return new Builder(new ItemStack(item));
+		}
+
+		public static Builder create(Block block, int meta)
+		{
+			return create(Item.getItemFromBlock(block), meta);
+		}
+
+		public static Builder create(Block block)
+		{
+			return create(Item.getItemFromBlock(block));
+		}
+
+		private Builder mani(Consumer<ItemStack> consumer) {
+			consumer.accept(this.itemStack);
+			return this;
+		}
+
+		private Builder named(String name) {
+			return this.mani(s -> s.setStackDisplayName(name));
+		}
+
+		private Builder potion(PotionType potionType) {
+			return this.mani(s -> PotionUtils.addPotionToItemStack(s, potionType));
+		}
+
+		private Builder enchant(Enchantment ench, int level) {
+			return this.mani(s -> s.addEnchantment(ench, level));
+		}
+
+		private Builder expensive() {
+			return this.mani(stack -> stack.setRepairCost(100));
+		}
+
+		private ItemStack get() {
+			return this.itemStack;
+		}
+	}
 }
