f53a450c6e10b01843f5b873a685fcbec60d8f30
Fallen_Breath <yqx791125833@gmail.com>
2021-06-14 15:33:56 +0800

    moral thing updating

--- tcuhcSrc/cn/topologycraft/uhc/util/PlayerItems.java
+++ tcuhcSrc/cn/topologycraft/uhc/util/PlayerItems.java
@@ -11,6 +11,8 @@ import net.minecraft.init.PotionTypes;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.FurnaceRecipes;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.potion.PotionType;
 import net.minecraft.potion.PotionUtils;
 
@@ -20,22 +22,31 @@ import java.util.function.Consumer;
 public class PlayerItems {
 
 	private static final Map<String, ItemStack> items = Maps.newLinkedHashMap();
+	private static final ItemStack DEFAULT_MORAL = new ItemStack(Items.PAPER);
+
+	private static void setSingleLore(ItemStack itemStack, String lore)
+	{
+		NBTTagList loreList = new NBTTagList();
+		loreList.appendTag(new NBTTagString(lore));
+		itemStack.getOrCreateSubCompound("display").setTag("Lore", loreList);
+	}
 
 	public static ItemStack getPlayerItem(String playerName, boolean onFire) {
-		ItemStack stack = items.get(playerName);
-		if (stack == null) stack = new ItemStack(Items.PAPER);
-		else stack = stack.copy();
+		ItemStack stack = items.getOrDefault(playerName, DEFAULT_MORAL).copy();
 		// drop smelted item if possible when the player is on fire
 		if (onFire) {
 			ItemStack smeltResult = FurnaceRecipes.instance().getSmeltingResult(stack);
 			if (!smeltResult.isEmpty()) {
+				if (stack.hasDisplayName()) {
+					smeltResult.setStackDisplayName(stack.getDisplayName());
+				}
 				stack = smeltResult;
 			}
 		}
 		// apply name
-		String moral = playerName + "'s moral(jie) integrity(cao)";
-		if (!stack.hasDisplayName()) stack.setStackDisplayName(moral);
-		else stack.setSingleLore(moral);
+		String moralDescription = playerName + "'s moral(jie) integrity(cao)";
+		if (!stack.hasDisplayName()) stack.setStackDisplayName(moralDescription);
+		else setSingleLore(stack, moralDescription);
 		// apply sharpness I if no enchantment
 		if (stack.getEnchantmentTagList().tagCount() == 0)
 			stack.addEnchantment(Enchantments.SHARPNESS, 1);
@@ -65,7 +76,7 @@ public class PlayerItems {
 		items.put("Dazo66", new ItemStack(Blocks.YELLOW_FLOWER));
 		items.put("Gamepiaynmo", new ItemStack(Items.COAL));
 		items.put("CCS_Covenant", new ItemStack(Items.COOKIE).setStackDisplayName("Crispy Crispy Shark!"));
-		items.put("Lancet_Corgi", Builder.create(Items.IRON_SWORD, 250).enchant(Enchantments.SHARPNESS, 5).expensive().get());
+		items.put("Lancet_Corgi", Builder.create(Items.IRON_SWORD, 250).named("Lancet").enchant(Enchantments.SHARPNESS, 5).expensive().get());
 		items.put("ajisai_iii", Builder.create(Items.CHICKEN).named("rua aji").get());  // rua!
 		items.put("Aschin", Builder.create(Items.WHEAT).named("comymy").get());
 		items.put("Dou_Bi_Long", Builder.create(Blocks.DRAGON_EGG).named("longbao no egg").enchant(Enchantments.UNBREAKING, 10).enchant(Enchantments.MENDING, 1).get());
@@ -76,14 +87,14 @@ public class PlayerItems {
 		items.put("hai_dan", Builder.create(Items.EGG).named("sea egg").get());
 		items.put("Fallen_Breath", Builder.create(Items.LEATHER_CHESTPLATE).mani(s -> Items.LEATHER_CHESTPLATE.setColor(s, 16742436)).named("fox fur coat").get());
 		items.put("Sanluli36li", Builder.create(Items.TNT_MINECART).named("36li's self-destruct-car").enchant(Enchantments.FORTUNE, 3).get());
-		items.put("shamreltuim", Builder.create(Items.FISH, 3).get());  // puffer fish
+		items.put("shamreltuim", Builder.create(Items.FISH, 3).enchant(Enchantments.BINDING_CURSE, 1).get());  // puffer fish
 		items.put("YtonE", Builder.create(Items.POTIONITEM).potion(PotionTypes.EMPTY).named("liquid ketone").get());  // uncraftable potion
 		items.put("DawNemo", Builder.create(Blocks.DOUBLE_PLANT, 0).named("Real Man Never Look Round").enchant(Enchantments.THORNS, 1).get());  // sunflower
 		items.put("Van_Nya", Builder.create(Items.RABBIT_STEW).named("Van Nya Stew").get());  // Nya? Nya!
 		items.put("youngdao", Builder.create(Items.STONE_SWORD, 131).named("Murasame").enchant(Enchantments.SHARPNESS, 10).expensive().get());
 		items.put("ql_Lwi", Builder.create(Items.COOKED_FISH).named("Dinner in the belly of a penguin").get());
 		items.put("Azulene0907", Builder.create(Items.SPLASH_POTION).potion(PotionTypes.EMPTY).named("ArBQ").get());  // uncraftable potion
-		items.put("LUZaLID", Builder.create(Items.DYE, 6).named("cyanLu").get());  // uncraftable potion
+		items.put("LUZaLID", Builder.create(Items.DYE, 6).named("cyanLu").get());  // cyan dye
 		items.put("U_ruby", Builder.create(Items.FEATHER).named("double u").get());
 		items.put("Do1phin_jump", Builder.create(Items.FISH, 2).named("do1phin's food").enchant(Enchantments.UNBREAKING, 3).get());
 	}
